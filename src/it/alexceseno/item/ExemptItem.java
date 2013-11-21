b027d3d6d53880a809bd24ff49375a4e1ead9325
Alessandro Ceseno <AlexJSE@users.noreply.github.com>
2013-11-21 14:20:17 +0100

    refactoring methods

--- src/it/alexceseno/item/ExemptItem.java
+++ src/it/alexceseno/item/ExemptItem.java
@@ -2,7 +2,7 @@ package it.alexceseno.item;
 
 public class ExemptItem extends Item{
 	
-	public static double TAX_SALES = 0.00;
+	private static double TAX_SALES = 0.00;
 	
 	public enum BASIC_SALES_EXEMPT{
 		BOOK, FOOD, MEDICAL_PRODUCT, CHOCOLATE, PILL; 
